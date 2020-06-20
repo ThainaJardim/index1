3c462990e8d5ac6bf25fc9f1349fa5318a4b9f88
ThainaDev <thainaj.aguiar@gmail.com>
2020-06-19 22:27:55 -0300

    explicando function

--- index1.js
+++ index1.js
@@ -1,9 +1,9 @@
 // Cria um programa que clacula a média 
-//das notas entre os alunos e envia
+//das turmas de alunos e envia
 // mensagem do cálculo da média.
 //Array ou vetores
 
-const alunos = [
+const alunosDaTurmaA = [
     {
         nome: 'Mayk',
         nota: 9.8
@@ -17,16 +17,41 @@ const alunos = [
     nota: 2
 },
 ]
+
+const alunosDaTurmaB = [
+    {
+        nome: 'Cleiton',
+        nota: 10
+    },
+{
+    nome: "Robson",
+    nota: 10
+}, 
+ { 
+    nome: "Siclano",
+    nota: 2
+},
+]
   
-const nomesDeAlunos = ['Mayk', 'Diego', 'Fulano']
+function calculaMedia(alunos) {
+    return (alunos[0].nota + alunos[1].nota + alunos[2].nota) / 3
 
-console.log (alunos)
+}
 
-const media = (alunos[0].nota + alunos[1].nota + alunos[2].nota) / 3
+calculaMedia(alunosDaTurmaA)
+calculaMedia(alunosDaTurmaB)
 
+const media1 = calculaMedia(alunosDaTurmaA)
+const media2 = calculaMedia(alunosDaTurmaB)
+
+function enviaMensagem(media, turma) {
 //Se a média for maoir que 5, parabenizar a turma
 if (media > 5) {
-    console.log (`A media foi de ${media}. Parabéns`)
+    console.log (`A media da turma ${turma} foi de ${media}. Parabéns`)
  } else {
-     console.log ('A media é menor que 5')
+     console.log (`A media da turma ${turma} é menor que 5`)
+     }
  }
+
+ enviaMensagem(media1,'turmaA') //aqui são parâmetros da função
+ enviaMensagem(media2,'turmaB')
